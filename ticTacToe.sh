05fa8337503919a029004359f266ff2d0d1b0ba5
kajal764 <kajaldw666@gmail.com>
2019-12-16 11:30:51 +0530

    Added: Print Statement To Initialized The Board

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -2,3 +2,10 @@
 
 echo "!!! WELCOME  TO  TIC-TAC-TOE GAME ... !!!"
 
+declare -a board
+board=( 1 2 3 4 5 6 7 8 9)
+
+echo " ${board[0]} ${board[1]} ${board[2]} " 
+echo " ${board[3]} ${board[4]} ${board[5]} " 
+echo " ${board[6]} ${board[7]} ${board[8]} " 
+
