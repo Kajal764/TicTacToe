2e8d4503d273e59b7ba8d16344dc0b3541913c54
kajal764 <kajaldw666@gmail.com>
2019-12-16 11:55:02 +0530

    Added: Random Function To Decide Player Symbol

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -3,8 +3,21 @@
 echo "!!! WELCOME  TO  TIC-TAC-TOE GAME ... !!!"
 
 declare -a board
-board=( 1 2 3 4 5 6 7 8 9 )
+function getInitializedBoard(){
+	board=( 1 2 3 4 5 6 7 8 9 )
 
-echo " ${board[0]} ${board[1]} ${board[2]} "
-echo " ${board[3]} ${board[4]} ${board[5]} "
-echo " ${board[6]} ${board[7]} ${board[8]} "
+	echo " ${board[0]} ${board[1]} ${board[2]} "
+	echo " ${board[3]} ${board[4]} ${board[5]} "
+	echo " ${board[6]} ${board[7]} ${board[8]} "
+}
+getInitializedBoard
+
+function getSymbol(){
+	if [[ $((RANDOM%2)) -eq 0 ]]
+	then
+		echo "player 1  Symbol is = X"
+	else
+		echo "player 1 Symbol is = O"
+	fi
+}
+getSymbol
