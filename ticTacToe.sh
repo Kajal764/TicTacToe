faf8bc3f438b326045fa2a6b1c3d9b3816191028
kajal764 <kajaldw666@gmail.com>
2019-12-17 10:55:40 +0530

    Added: Functions To Check Winning Condition

--- ticTacToe.sh
+++ ticTacToe.sh
@@ -3,6 +3,9 @@
 echo "!!! WELCOME  TO  TIC-TAC-TOE GAME ... !!!"
 
 count=0
+flag1=0
+flag2=0
+flag3=0
 
 declare -a board
 board=( 1 2 3 4 5 6 7 8 9 )
@@ -27,23 +30,67 @@ function getBoardPrint(){
 	
 }
 getBoardPrint
+
 function getCellNum(){
-	read -p "Enter Cell Num  " cellNum 
+	read -p "Enter Cell Number  " cellNum 
 	for (( i=0;i<=8;i++ ))
 	do
-		if [[ ${board[$i]}  == $cellNum ]]
+		if [[ ${board[$i]} -eq $cellNum ]]
 		then
 			board[$(($cellNum-1))]="X"
 			count=$(($count+1))
 			break
 		fi
-		
 	done
+}
+
+function checkForRow(){
+	for((i=0;i<=8;i=$(($i+3)) ))
+   do
+      if [[ ${board[$i]} == "X"  && ${board[$(($i+1))]} == "X"  && ${board[$(($i+2))]} == "X" ]]
+      then
+         flag1=1
+         break
+		fi
+	done
+	echo $flag1
+}
+
+function checkForColumn(){
+	for(( i=0;i<=2;i++ ))
+   do
+		if [[ ${board[$i]} == "X"  && ${board[$(($i+3))]} == "X"  && ${board[$(($i+6))]} == "X" ]]
+      then
+			flag2=1
+			break
+		fi
+   done
+   echo $flag2
+}
+
+function checkForDiagonal(){
+	i=0
+	if [[ ${board[$i]} == "X"  && ${board[$(($i+4))]} == "X"  && ${board[$(($i+8))]} == "X" ]]
+   then
+		flag3=1
+	elif [[  ${board[$i+2]} == "X"  && ${board[$(($i+4))]} == "X"  && ${board[$(($i+6))]} == "X" ]]
+   then	
+			flag3=1
+	fi
+	echo $flag3
 }
 
 while [[ $count -ne 9 ]]
 do
 	getCellNum
 	getBoardPrint
+	rowValue=$(checkForRow)
+	columnValue=$(checkForColumn)
+	diagonalValue=$(checkForDiagonal)
+	if [[ $rowValue -eq 1 || $columnValue -eq 1 || $diagonalValue -eq 1 ]]
+	then
+		echo "X Symbol User Win!!!"
+		break
+	fi
 done
 
